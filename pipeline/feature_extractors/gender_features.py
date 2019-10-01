391f1a7841a3ac30ffe4de072ab499d65dcde47e
Syavaprd <v.a._denisov@mail.ru>
2019-10-02 00:11:25 +0300

    Fix

--- pipeline/feature_extractors/gender_features.py
+++ pipeline/feature_extractors/gender_features.py
@@ -9,6 +9,7 @@ class FeatureExtractorCustomerAge(FeatureExtractorBase):
 
     def extract(self,  transactions: pd.DataFrame, stories: pd.DataFrame, users: pd.DataFrame) -> pd.DataFrame:
         gender = users[['gender_rk', 'customer_id']]
-        gender = gender.rename({'gender_rk': 'customer_gender'})
+        gender = gender.rename(columns={'gender_cd': 'customer_gender'})
+        gender['customer_gender'] = [0 if gen == 'M' else 1 for gen in gender['customer_gender']]
         stories = stories.merge(gender, on='customer_id', how='left')
         return stories
