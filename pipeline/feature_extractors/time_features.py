263e2472c127e434a401b5a96936cce3729566e2
Syavaprd <v.a._denisov@mail.ru>
2019-10-03 01:35:24 +0300

    Time features

---
+++ pipeline/feature_extractors/time_features.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datetime import datetime
+
+from .base import FeatureExtractorBase
+
+
+class FeatureExtractorHourCategory(FeatureExtractorBase):
+    def extract(self, transactions: pd.DataFrame, stories: pd.DataFrame, users: pd.DataFrame, candidates: pd.DataFrame) -> pd.DataFrame:
+        candidates['event_datetime'] = [datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in candidates.event_dttm]
+        candidates['hour_category'] = [x.hour for x in candidates.event_datetime]
+        candidates.drop(columns=['event_datetime'], inplace=True)
+        candidates.hour_category = candidates.hour_category.astype('category')
+        return candidates
+
+
+class FeatureExtractorDayCategory(FeatureExtractorBase):
+    def extract(self, transactions: pd.DataFrame, stories: pd.DataFrame, users: pd.DataFrame, candidates: pd.DataFrame) -> pd.DataFrame:
+        candidates['event_datetime'] = [datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in candidates.event_dttm]
+        candidates['week_day'] = [x.weekday() for x in candidates.event_datetime]
+        candidates.drop(columns=['event_datetime'], inplace=True)
+        candidates.event_datetime = candidates.event_datetime.astype('category')
+        return candidates
+
