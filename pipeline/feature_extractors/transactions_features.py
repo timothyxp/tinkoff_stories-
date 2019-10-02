77197dbbcc331c9528e11078eb635b3923e68da1
Unknown <emsidorov@gmail.com>
2019-10-02 22:43:20 +0300

    New features on transactions

--- pipeline/feature_extractors/transactions_features.py
+++ pipeline/feature_extractors/transactions_features.py
@@ -35,3 +35,61 @@ class FeatureExtractorAvgTransactionAmt(FeatureExtractorBase):
         candidates = candidates.merge(user_transactions_mean, on='customer_id', how='left')
         candidates = candidates.rename(columns={'transaction_amt': 'customer_avg_amt'})
         return candidates
+
+
+class FeatureExtractorAvgTransactionAmtByMonth(FeatureExtractorBase):
+    def extract(self, transactions: pd.DataFrame, stories: pd.DataFrame, users: pd.DataFrame, candidates: pd.DataFrame) -> pd.DataFrame:
+        user_transactions = transactions[['customer_id', 'transaction_amt']]
+        for i in [5, 6, 7]:
+            transactions_mean_bill_month = transactions[transactions['transaction_month'] == i].groupby(['customer_id'])[
+                'transaction_amt'].mean().reset_index()
+            transactions_mean_bill_month = transactions_mean_bill_month.rename(
+                columns={'transaction_amt': 'transaction_amt_mean_month_' + str(i)})
+            user_transactions = user_transactions.merge(transactions_mean_bill_month, on=['customer_id'], how='left')
+        user_transactions = user_transactions.drop(['transaction_amt'], axis=1)
+        return user_transactions
+
+
+class FeatureExtractorAvgMerchantUnique(FeatureExtractorBase):
+    def extract(self, transactions: pd.DataFrame, stories: pd.DataFrame, users: pd.DataFrame, candidates: pd.DataFrame) -> pd.DataFrame:
+        user_transactions = transactions[['customer_id', 'merchant_id', 'merchant_mcc']].copy()
+        user_transactions_count_unique_id = user_transactions.groupby(
+            ['customer_id'])['merchant_id'].nunique().reset_index()
+
+        user_transactions_count_unique_id = user_transactions_count_unique_id.rename(
+            columns={'merchant_id': 'merchant_id_unique'})
+
+        user_transactions_count_unique_mcc  = user_transactions.groupby(
+            ['customer_id'])['merchant_mcc'].nunique().reset_index()
+
+        user_transactions_count_unique_mcc = user_transactions_count_unique_mcc.rename(
+            columns={'merchant_id': 'merchant_mcc_unique'})
+
+        user_transactions = user_transactions.merge(user_transactions_count_unique_id, on=['customer_id'], how='left')
+        user_transactions = user_transactions.merge(user_transactions_count_unique_mcc, on=['customer_id'], how='left')
+        user_transactions = user_transactions.drop(['merchant_id', 'merchant_mcc'], axis=1)
+        return user_transactions
+
+
+class FeatureExtractorAvgMeanTransactionAmtOnMerchant(FeatureExtractorBase):
+    def extract(self, transactions: pd.DataFrame, stories: pd.DataFrame, users: pd.DataFrame, candidates: pd.DataFrame) -> pd.DataFrame:
+        transactions_merchant = transactions.groupby(['merchant_id'])['transaction_amt'].mean().reset_index()
+        transactions_merchant = transactions_merchant.rename(columns={'transaction_amt': 'transaction_amt_by_merchant'})
+        transactions_merchant_mean = transactions.copy()
+        transactions_merchant_mean = transactions_merchant_mean.merge(transactions_merchant, on=['merchant_id'],
+                                                                      how='left')
+        transactions_merchant_mean_customer_id = transactions_merchant_mean.groupby(['customer_id'])[
+            'transaction_amt_by_merchant'].mean().reset_index()
+
+        user_transactions = transactions.copy()
+        user_transactions = user_transactions.merge(transactions_merchant_mean_customer_id, on=['customer_id'],
+                                                    how='left')
+
+        return user_transactions[['customer_id', 'transaction_amt_by_merchant']]
+
+
+
+
+
+
+
