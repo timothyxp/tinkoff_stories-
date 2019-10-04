e29afb8869ce9ec21546d85cc8713cd4f29be8d7
timothyxp <timothyxp@mail.ru>
2019-10-04 09:05:59 +0300

    add static from description

--- pipeline/feature_extractors/descriptions.py
+++ pipeline/feature_extractors/descriptions.py
@@ -1,6 +1,7 @@
 from .base import FeatureExtractorBase
 import pandas as pd
 from pipeline.config_base import ConfigBase
+from colour import Color
 
 
 class FeatureExtractorStaticDescriptions(FeatureExtractorBase):
@@ -9,10 +10,19 @@ class FeatureExtractorStaticDescriptions(FeatureExtractorBase):
 
     def extract(self, transactions: pd.DataFrame, stories: pd.DataFrame, users: pd.DataFrame, candidates: pd.DataFrame) -> pd.DataFrame:
         description = pd.read_csv(self.config.stories_descriptions_parsed) \
-            ["story_id", "icon_alpha_0"]
+            [["story_id", "icon_alpha_0", "icon_hex_0", "icon_hex_1"]]
 
         candidates = candidates \
             .merge(description, on="story_id", how="left")
+
+        candidates["icon_0_red"] = [Color(hex).rgb[0] for hex in candidates["icon_hex_0"]]
+        candidates["icon_0_green"] = [Color(hex).rgb[1] for hex in candidates["icon_hex_0"]]
+        candidates["icon_0_blue"] = [Color(hex).rgb[2] for hex in candidates["icon_hex_0"]]
+        candidates["icon_0_red"] = [Color(hex).rgb[0] for hex in candidates["icon_hex_1"]]
+        candidates["icon_0_green"] = [Color(hex).rgb[1] for hex in candidates["icon_hex_1"]]
+        candidates["icon_0_blue"] = [Color(hex).rgb[2] for hex in candidates["icon_hex_1"]]
+
+        candidates.drop(columns=["icon_hex_0", "icon_hex_1"], inplace=True)
 
         return candidates
 
