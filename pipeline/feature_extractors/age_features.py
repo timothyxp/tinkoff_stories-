ad6b373a4d70c8f715aef478a0bb71551c87a59f
timothyxp <timothyxp@mail.ru>
2019-10-05 15:30:10 +0300

    fix

--- pipeline/feature_extractors/age_features.py
+++ pipeline/feature_extractors/age_features.py
@@ -4,11 +4,11 @@ from .base import FeatureExtractorBase
 
 
 def categorize_age(row):
-    if row.age <= 18:
+    if row.customer_age <= 18:
         return 0
-    if 18 < row.age <= 26:
+    if 18 < row.customer_age <= 26:
         return 1
-    if 26 < row.age <= 44:
+    if 26 < row.customer_age <= 44:
         return 2
     return 3
 
@@ -17,8 +17,8 @@ class FeatureExtractorAgeCategoryAndAge(FeatureExtractorBase):
     def extract(self, transactions: pd.DataFrame, stories: pd.DataFrame, users: pd.DataFrame,
                 candidates: pd.DataFrame) -> pd.DataFrame:
         age = users[['age', 'customer_id']].drop_duplicates(subset=['customer_id'])
-        age = age.rename(columns={{'age': 'customer_age'}})
-        age['age_category'] = age.apply(lambda row: categorize_age(row))
+        age = age.rename(columns={'age': 'customer_age'})
+        age['age_category'] = age.apply(lambda row: categorize_age(row), axis=1)
         age.age_category = age.age_category.astype('category')
         candidates = candidates.merge(age, on='customer_id', how='left')
         return candidates
